ddd9b2dfe913f4790d31adf82889ae5844de3c51
Zinx <saberjinx@outlook.com>
2024-08-10 17:28:55 +0530

    Cities Dropdown

--- js/main.js
+++ js/main.js
@@ -1,27 +1,52 @@
 const cont = document.querySelector('h1');
 const mon = document.querySelector('h2');
 const weather = document.querySelector('.weather');
+const drop_cities = document.querySelector('select');
 
-const dat = async (reso) => {
-    const res = await fetch(reso);
+const dat = async (lon,lat) => {
+    const res = await fetch(`http://www.7timer.info/bin/api.pl?lon=${lon}&lat=${lat}&product=civil&output=json`);
     const temp = await res.json();
-    console.log(temp);
     return temp;
 }
 
+
+const dropdown = async () =>{
+    const response = await fetch('/data/data.json');
+    const ttt = await response.json();
+    
+    ttt.forEach(city => {
+        const obj = document.createElement('option');
+        obj.value = city.id;
+        obj.textContent = city.place;
+        drop_cities.appendChild(obj);
+    })
+}
+
+
+dropdown();
 
 const month = new Date().getMonth() + 1;
 const date = new Date().getDate();
 
 const time = new Date().getHours();
+const minute = new Date().getMinutes();
 
 // const ll = new Date().toJSON();
 // console.log(typeof(ll));
 
-mon.innerHTML = date + '  ' + 'August' + `<br><br>` + time + ' AM';
+mon.innerHTML = date + '  ' + 'August' + `<br><br>` + time + ':' + minute + ' AM';
 
-const update = async (location) => {
-    const data = await dat(location);
+
+
+
+
+
+const update = async (id) => {
+    const response = await fetch('/data/data.json');
+    const ttt = await response.json();
+    const lon = ttt[id].lon;
+    const lat = ttt[id].lat;
+    const data = await dat(lon,lat);
     const newData = `
         <h3><b>Temperature : </b> ${data.dataseries[0].temp2m}°C</h3>
         <h3><b>Humidity : </b> ${data.dataseries[0].rh2m}</h3>
@@ -32,11 +57,10 @@ const update = async (location) => {
     weather.innerHTML = newData;
 }
 
-// update('http://www.7timer.info/bin/api.pl?lon=2.168&lat=41.387&product=civil&output=json');
-update('http://www.7timer.info/bin/api.pl?lon=83.433&lat=26.731&product=civil&output=json');
+update(1);
 
 
-window.addEventListener('DOMContentLoaded',() => dat('http://www.7timer.info/bin/api.pl?lon=2.168&lat=41.387&product=civil&output=json'));
-window.addEventListener('DOMContentLoaded',() => dat('http://www.7timer.info/bin/api.pl?lon=2.168&lat=41.387&product=astro&output=json'));
-window.addEventListener('DOMContentLoaded',() => dat('http://www.7timer.info/bin/api.pl?lon=-2.935&lat=43.263&product=civil&output=json'));
-window.addEventListener('DOMContentLoaded',() => dat('http://www.7timer.info/bin/api.pl?lon=-2.935&lat=43.263&product=astro&output=json'));
+// window.addEventListener('DOMContentLoaded',() => dat('http://www.7timer.info/bin/api.pl?lon=2.168&lat=41.387&product=civil&output=json'));
+// window.addEventListener('DOMContentLoaded',() => dat('http://www.7timer.info/bin/api.pl?lon=2.168&lat=41.387&product=astro&output=json'));
+// window.addEventListener('DOMContentLoaded',() => dat('http://www.7timer.info/bin/api.pl?lon=-2.935&lat=43.263&product=civil&output=json'));
+// window.addEventListener('DOMContentLoaded',() => dat('http://www.7timer.info/bin/api.pl?lon=-2.935&lat=43.263&product=astro&output=json'));
